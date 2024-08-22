0f8d063820498bbb6f71d1ba31832eab8de2bf2c
Saikiran-Rai <142512211+Saikiran-Rai@users.noreply.github.com>
2024-08-22 12:23:08 +0530

    VirtualMouseController.py

--- VirtualMouseController.py
+++ VirtualMouseController.py
@@ -35,39 +35,41 @@ while True:
 
     # GETTING THE TIP OF INDEX AND MIDDLE FINGERS
     if len(lm_list) != 0:
-        x1, y1 = lm_list[8][1:]
-        x2, y2 = lm_list[12][1:]
-        cv2.rectangle(cam, (frameR, frameR), (wcam-frameR, hcam-frameR), (0, 255, 0), 2)
-
-        # CONVERTING COORDINATES FROM CAM SIZE TO SCREEN SIZE
-        fingers = detector.fingersUp()
-        if fingers[1] == 1 and fingers[2] == 0:
-            x3 = np.interp(x1, (frameR, wcam-frameR), (0, wscr))
-            y3 = np.interp(y1, (frameR, hcam-frameR), (0, hscr))
-
-            # SMOOTHENING MOVEMENT
-            cLocX = pLocX + (x3 - pLocX) / smoothening
-            cLocY = pLocY + (y3 - pLocY) / smoothening
-
-            # MOVING MOUSE
-            pg.moveTo(wscr-cLocX, cLocY)
-            cv2.circle(cam, (x1, y1), 10, (0, 255, 0), 3, cv2.FILLED)
-            pLocX, pLocY = cLocX, cLocY
-
-        # FOR LEFT CLICK
-        if fingers[1] == 1 and fingers[2] == 1:
-            length, cam, fd_info = detector.findDistance(8, 12, cam)
-            if length < 40:
-                cv2.circle(cam, (fd_info[4], fd_info[5]), 10, (0, 255, 0), 3, cv2.FILLED)
-                pg.click(button="left", interval=0.5)
-
-        # FOR RIGHT CLICK
-        '''
-        if fingers[1] == 1 and fingers[4] == 1:
-            length, cam, fd_info = detector.findDistance(8, 4, cam)
-            if length < 30:
-                cv2.circle(cam, (fd_info[4], fd_info[5]), 10, (0, 255, 0), 3, cv2.FILLED)
-                pg.rightClick(interval=0.5)'''
+        area = ((bound_box[2]-bound_box[0])*(bound_box[3]-bound_box[1]))//100
+        if (area > 300) and (area < 1400):
+            x1, y1 = lm_list[8][1:]
+            x2, y2 = lm_list[12][1:]
+            cv2.rectangle(cam, (frameR, frameR), (wcam-frameR, hcam-frameR), (0, 255, 0), 2)
+    
+            # CONVERTING COORDINATES FROM CAM SIZE TO SCREEN SIZE
+            fingers = detector.fingersUp()
+            if fingers[1] == 1 and fingers[2] == 0:
+                x3 = np.interp(x1, (frameR, wcam-frameR), (0, wscr))
+                y3 = np.interp(y1, (frameR, hcam-frameR), (0, hscr))
+    
+                # SMOOTHENING MOVEMENT
+                cLocX = pLocX + (x3 - pLocX) / smoothening
+                cLocY = pLocY + (y3 - pLocY) / smoothening
+    
+                # MOVING MOUSE
+                pg.moveTo(wscr-cLocX, cLocY)
+                cv2.circle(cam, (x1, y1), 10, (0, 255, 0), 3, cv2.FILLED)
+                pLocX, pLocY = cLocX, cLocY
+    
+            # FOR LEFT CLICK
+            if fingers[1] == 1 and fingers[2] == 1:
+                length, cam, fd_info = detector.findDistance(8, 12, cam)
+                if length < 40:
+                    cv2.circle(cam, (fd_info[4], fd_info[5]), 10, (0, 255, 0), 3, cv2.FILLED)
+                    pg.click(button="left", interval=0.5)
+    
+            # FOR RIGHT CLICK
+            '''
+            if fingers[1] == 1 and fingers[4] == 1:
+                length, cam, fd_info = detector.findDistance(8, 4, cam)
+                if length < 30:
+                    cv2.circle(cam, (fd_info[4], fd_info[5]), 10, (0, 255, 0), 3, cv2.FILLED)
+                    pg.rightClick(interval=0.5)'''
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
